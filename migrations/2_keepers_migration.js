58cd344cfda22f645fc7fba332d539f1bd65e34f
charliedevcode <charliedevcode@gmail.com>
2023-04-09 16:01:43 +0200

    migration

--- migrations/2_keepers_migration.js
+++ migrations/2_keepers_migration.js
@@ -2,9 +2,8 @@ const Keepers = artifacts.require("Keepers");
 module.exports = async function (deployer, accounts) {
   //0xeA311A78F8F3Ba204808cE56c56be677f6A6ae74
   // 0x4E48D90085B3CDE260f91b2863718bc28282dF8f
-  const COUNCIL_ADDRESS = "0x7A724caFb3A04eC38eb92A88aB42536802E4241e";
+  const COUNCIL_ADDRESS = "0x33AfbEBbD4F03f7AFeE4181F61d49Ec94801a32f";
   const ORACLE_NODE_ADDRESS = "0xD9dDCdD3100630ac357f63fB9353f576fD3C9533"
 
   deployer.deploy(Keepers, COUNCIL_ADDRESS, ORACLE_NODE_ADDRESS);
-
 };
