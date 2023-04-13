06981d056d54bdd9dd5c4ba2c036b413cf2500a0
charliedevcode <charliedevcode@gmail.com>
2023-04-13 21:12:05 +0200

    made literal in amount (ether) = 10**18

--- contracts/CSX/CSX.sol
+++ contracts/CSX/CSX.sol
@@ -4,8 +4,8 @@ pragma solidity 0.8.19;
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
 contract CSXToken is ERC20Burnable {
-    // ONE HUNDERED MILLION CSX
-    uint256 public constant maxSupply = 100000000 ether;
+    // ONE HUNDERED MILLION CSX TOKENS
+    uint256 public constant maxSupply = 100000000 * 10**18;
     IERC20 public stakedCSX;
 
     constructor(address stakedCSXAddress) ERC20("CSX Token", "CSX") {
