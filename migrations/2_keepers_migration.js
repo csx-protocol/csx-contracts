e423678cf53fefe021a636710f05c7a097759e87
charliedevcode <charliedevcode@gmail.com>
2023-04-14 15:59:59 +0200

    redundant

--- migrations/2_keepers_migration.js
+++
@@ -1,9 +0,0 @@
-const Keepers = artifacts.require("Keepers");
-module.exports = async function (deployer, accounts) {
-  //0xeA311A78F8F3Ba204808cE56c56be677f6A6ae74
-  // 0x4E48D90085B3CDE260f91b2863718bc28282dF8f
-  const COUNCIL_ADDRESS = "0x3A5e13F544CD52cD9839993bCAf168680a674265";
-  const ORACLE_NODE_ADDRESS = "0xD9dDCdD3100630ac357f63fB9353f576fD3C9533"
-
-  deployer.deploy(Keepers, COUNCIL_ADDRESS, ORACLE_NODE_ADDRESS);
-};
--- migrations/3_users_migration.js
+++
@@ -1,7 +0,0 @@
-const Users = artifacts.require("Users");
-const Keepers = artifacts.require("Keepers");
-
-module.exports = async function  (deployer) {
-  console.log("KEEPERS ADDy", Keepers.address);
-  await deployer.deploy(Users, Keepers.address);
-};
