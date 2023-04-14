9d68c90dac4b0fcb622ab78c780b895c236c944d
charliedevcode <charliedevcode@gmail.com>
2023-04-14 15:55:45 +0200

    license + prettier

--- contracts/CSX/CSX.sol
+++ contracts/CSX/CSX.sol
@@ -1,15 +1,14 @@
-//SPDX-License-Identifier: Unlicense
+// SPDX-License-Identifier: MIT
+// CSX Token Contract v1
 pragma solidity 0.8.19;
 
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
 contract CSXToken is ERC20Burnable {
-    // ONE HUNDERED MILLION CSX TOKENS
-    uint256 public constant maxSupply = 100000000 * 10**18;
-    IERC20 public stakedCSX;
+    // ONE HUNDERED MILLION CSX TOKENS WITH 18 DECIMALS
+    uint256 public constant maxSupply = 100000000 * 10 ** 18;
 
-    constructor(address stakedCSXAddress) ERC20("CSX Token", "CSX") {
+    constructor() ERC20("CSX Token", "CSX") {
         _mint(msg.sender, maxSupply);
-        stakedCSX = IERC20(stakedCSXAddress);
-    }    
+    }
 }
