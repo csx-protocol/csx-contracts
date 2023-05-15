b4df74300ef91fedaa583c2b45fbe3f61040a8b7
iam-dev <>
2023-05-15 17:13:01 +0200

    Add more Foundry test

--- test/CSX/CSX.t.sol
+++ test/CSX/CSX.t.sol
@@ -2,8 +2,8 @@
 
 pragma solidity ^0.8.19;
 
-import "forge-std/Test.sol";
-import "contracts/CSX/CSX.sol";
+import { Test } from "forge-std/Test.sol";
+import { CSXToken } from "contracts/CSX/CSX.sol";
 
 contract CSXTokenTest is Test {
     CSXToken public csx;
@@ -12,6 +12,9 @@ contract CSXTokenTest is Test {
 
     uint256 public constant maxSupply = 100000000;
 
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+    
     function setUp() public {
         vm.prank(deployer);
         csx = new CSXToken("CSX Token", "CSX", maxSupply);
@@ -40,14 +43,44 @@ contract CSXTokenTest is Test {
         assertEq(csx.balanceOf(user), 0);
     }
 
-    function testTransfer(address user, uint256 amount) public {
-        vm.assume(user != address(0));
-        vm.assume(user != deployer);
+    function testTransfer(address to, uint256 amount) public {
+        vm.assume(to != address(0));
+        vm.assume(to != deployer);
         vm.assume(amount > 0);
         vm.assume(amount <= maxSupply);
+        vm.expectEmit(true, true, false, true);
         vm.prank(deployer);
-        csx.transfer(user, amount);
+        csx.transfer(to, amount);
+        emit Transfer(deployer, to, amount);
         vm.stopPrank();
-        assertEq(csx.balanceOf(user), amount);
+        assertEq(csx.balanceOf(to), amount);
+    }
+
+    function testApprove(address approver, address spender, uint256 amount) public {
+        vm.assume(approver != address(0));
+        vm.assume(spender != address(0));
+        vm.assume(amount > 0);
+        vm.assume(amount <= maxSupply);
+        vm.expectEmit(true, true, false, true);
+        vm.prank(approver);
+        csx.approve(spender, amount);
+        emit Approval(approver, spender, amount);
+        vm.stopPrank();
+        assertEq(csx.allowance(approver, spender), amount);
+    }
+
+    function testTransferFrom(address approver, address spender, uint256 amount) public {
+        vm.assume(approver != deployer);
+        vm.assume(spender != deployer);
+        vm.assume(approver != address(0));
+        vm.assume(spender != address(0));
+        vm.assume(approver != spender);
+        testTransfer(approver, amount);
+        testApprove(approver, spender, amount);
+        vm.expectEmit(true, true, false, true);
+        vm.prank(spender);
+        csx.transferFrom(approver, spender, amount);
+        emit Transfer(approver, spender, amount);
+        assertEq(csx.balanceOf(spender), amount);
     }
 }
