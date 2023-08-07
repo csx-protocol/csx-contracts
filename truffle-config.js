a57593e73ec356bbdf20417d5cc6a6046c547680
charliedevcode <charliedevcode@gmail.com>
2023-08-07 21:56:32 +0200

    cli dev env

--- truffle-config.js
+++ truffle-config.js
@@ -46,6 +46,16 @@ module.exports = {
     // tab if you use this network and you must also set the `host`, `port` and `network_id`
     // options below to some value.
     //
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*"
+    },
+    // test: {
+    //   host: "127.0.0.1",
+    //   port: 7545,
+    //   network_id: "*"
+    // },
     // development: {
     //  host: "127.0.0.1",     // Localhost (default: none)
     //  port: 8545,            // Standard Ethereum port (default: none)
