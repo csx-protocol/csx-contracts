6743419bc51cad24dbb5e048472f4c759d58f97f
charliedevcode <charliedevcode@gmail.com>
2023-08-22 19:43:41 +0200

    specified importing & rem redudant code

--- contracts/CSX/EscrowedCSX.sol
+++ contracts/CSX/EscrowedCSX.sol
@@ -3,24 +3,15 @@
 
 pragma solidity 0.8.19;
 
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import {ERC20Burnable, ERC20, IERC20} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
 import {ReentrancyGuard} from "./Interfaces.sol";
 
 contract EscrowedCSX is ERC20Burnable, ReentrancyGuard {
     address deployer = msg.sender;
-    bool isInitialized = false;
+    bool public isInitialized = false;
     IERC20 public csxToken;
-    IERC20 public vestingToken;
-
-    // uint256 public constant VESTING_PERIOD = 24 * 30 days;
-
-    // struct Vesting {
-    //     uint256 amount;
-    //     uint256 startTime;
-    // }
-
-    // mapping(address => Vesting) public vestings;
+    IERC20 public vestedCSX;
 
     event Minted(address indexed user, uint256 amount);
     event Claimed(address indexed user, uint256 amount);
@@ -33,26 +24,15 @@ contract EscrowedCSX is ERC20Burnable, ReentrancyGuard {
         require(!isInitialized, "Already initialized");
         require(msg.sender == deployer, "Only deployer can initialize");
         isInitialized = true;
-        vestingToken = IERC20(_vCSXToken);
+        vestedCSX = IERC20(_vCSXToken);
     }
 
     function mintEscrow(uint256 _amount) external nonReentrant {
         require(_amount > 0, "Amount must be greater than 0");
-        require(csxToken.transferFrom(msg.sender, address(vestingToken), _amount), 'Transfer failed.');
+        require(csxToken.transferFrom(msg.sender, address(vestedCSX), _amount), 'Transfer failed.');
         _mint(msg.sender, _amount);
         // vestings[msg.sender] = Vesting(_amount, block.timestamp);
         // handle vesting in vCSX contract
         emit Minted(msg.sender, _amount);
     }
-
-    // function claim() external nonReentrant {
-    //     require(vestings[msg.sender].amount > 0, "No vested tokens found");
-    //     require(block.timestamp >= vestings[msg.sender].startTime + VESTING_PERIOD, "Vesting period not completed");
-
-    //     uint256 amount = vestings[msg.sender].amount;
-    //     delete vestings[msg.sender];
-    //     _burn(msg.sender, amount);
-    //     csxToken.transfer(msg.sender, amount);
-    //     emit Claimed(msg.sender, amount);
-    // }
 }
