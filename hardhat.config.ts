7b0692c34658b6505291d708853bdab2bd0e1c37
charliedevcode <charliedevcode@gmail.com>
2023-09-08 13:02:17 +0200

    hardhat init

---
+++ hardhat.config.ts
@@ -0,0 +1,42 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import dotenv from 'dotenv';
+dotenv.config();
+
+const privateKeyDev = process.env.HEXKEY!;
+const rpcLink = process.env.RPCURL;
+
+const config: HardhatUserConfig = {
+  defaultNetwork: "hardhat",  
+  networks: {
+    hardhat: {
+    },
+    ganache: {
+      url: "http://127.0.0.1:7545",  // Localhost (default: none)
+      // accounts: {
+      //   mnemonic: "test test test test test test test test test test test junk" // Replace with your Ganache mnemonic or private key
+      // },
+      // gas: 8721974,
+      chainId: 1337,
+    },
+    goerli: {
+      url: rpcLink,
+      accounts: [privateKeyDev],
+      gasPrice: 1000000000,
+      chainId: 421613,
+      timeout: 200000
+    }
+  },
+  solidity: {
+    version: "0.8.19",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 300
+      },
+      // evmVersion: "byzantium"
+    }
+  }
+};
+
+export default config;
