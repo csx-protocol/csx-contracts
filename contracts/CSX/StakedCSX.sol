bd23c7f0bfce9342b888c7ccf9b337d502928625
charliedevcode <charliedevcode@gmail.com>
2023-09-25 18:19:27 +0200

    https://github.com/crytic/slither/wiki/Detector-Documentation#name-reused

--- contracts/CSX/StakedCSX.sol
+++ contracts/CSX/StakedCSX.sol
@@ -6,14 +6,7 @@ import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol
 import {ReentrancyGuard} from "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {IKeepers} from "../Keepers/IKeepers.sol";
-
-interface IWETH is IERC20 {
-    function deposit() external payable;
-
-    function transfer(address to, uint value) external returns (bool);
-
-    function withdraw(uint) external;
-}
+import {IWETH} from "../Interfaces.sol";
 
 contract StakedCSX is ReentrancyGuard, ERC20 {
     using SafeERC20 for IERC20;
