77e2a5ec5c64b0b7206459e031636e2026cdd32a
charliedevcode <charliedevcode@gmail.com>
2023-09-25 20:52:24 +0200

    using safe from interface

--- contracts/CSX/Interfaces.sol
+++ contracts/CSX/Interfaces.sol
@@ -4,6 +4,7 @@ pragma solidity 0.8.19;
 import {IERC20} from "@openzeppelin/contracts/interfaces/IERC20.sol";
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {ReentrancyGuard} from "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 interface IWETH is IERC20 {
     function deposit() external payable;
--- contracts/CSX/StakedCSX.sol
+++ contracts/CSX/StakedCSX.sol
@@ -2,11 +2,10 @@
 pragma solidity ^0.8.19;
 
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {ReentrancyGuard} from "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {IKeepers} from "../Keepers/IKeepers.sol";
-import {IWETH} from "./Interfaces.sol";
+import {IWETH, SafeERC20} from "./Interfaces.sol";
 
 contract StakedCSX is ReentrancyGuard, ERC20 {
     using SafeERC20 for IERC20;
