284e86941f25aa192d409eef8644b07679deb652
charliedevcode <charliedevcode@gmail.com>
2023-09-29 13:19:28 +0200

    added 1 more test listing

--- test/tradeFactory/CSXTradeFactory.test.ts
+++ test/tradeFactory/CSXTradeFactory.test.ts
@@ -38,12 +38,12 @@ describe("TradeFactory", function () {
         [],
         [],
     ];
-    const priceTypes = ['0', '0', '1', '0', '0', '2', '0'];
-    const prices = ['2977130000000000', '8933480000000000', '27800000', '44661960000000000', '89323910000000000', '457000000', '1489077140000000000'];
-    const names = ['R8 Revolver | Bone Mask (Field-Tested)', 'Negev | Bulkhead (Minimal Wear)', 'Negev | Army Sheen (Minimal Wear)', 'MAC-10 | Nuclear Garden (Field-Tested)', 'Galil AR | Rocket Pop (Factory New)', 'StatTrak™ AK-47 | Slate (Factory New)', '★ Driver Gloves | Rezan the Red (Minimal Wear)']
-    const weaponTypes = ['R8 Revolver', 'Negev', 'Negev', 'MAC-10', 'Galil AR', 'AK-47', 'Driver Gloves'];
-    const imgs = ['https://media.steampowered.com/apps/730/icons/econ/default_generated/weapon_revolver_sp_tape_light_large.c8f9124ff70ca2a6e8867920cd39e4fb7308ac87.png', 'https://media.steampowered.com/apps/730/icons/econ/default_generated/weapon_negev_hy_ducts_yellow_light_large.9d9335325a4a696ec6c2ef704ec1d4b3112c8c87.png', 'https://media.steampowered.com/apps/730/icons/econ/default_generated/weapon_negev_am_army_shine_light_large.884085f4a13b786f0ac7234d616ff01a848f28d5.png', 'https://media.steampowered.com/apps/730/icons/econ/default_generated/weapon_mac10_am_nuclear_skulls3_mac10_light_large.467b325065522e5248247cf125bec257cdb66902.png', 'https://media.steampowered.com/apps/730/icons/econ/default_generated/weapon_galilar_cu_galilar_particles_light_large.8732f64d53dbc9b0c732641655d4f99124d8cacc.png', 'https://media.steampowered.com/apps/730/icons/econ/default_generated/weapon_ak47_gs_ak47_professional_light_large.d09d623d0a725c63e8a3905f66bba41ba2ed59e8.png', 'https://media.steampowered.com/apps/730/icons/econ/default_generated/slick_gloves_slick_rezan_light_large.642934831085e8715a7e8072614f71f9fc0f205e.png']
-    const inspctLink = ['steam://rungame/730/76561202255233023/+csgo_econ_action_preview%20S76561198185748194A28591758742D769815130885352460', 'steam://rungame/730/76561202255233023/+csgo_econ_action_preview%20S76561198185748194A28492787574D16143929557675144168', 'steam://rungame/730/76561202255233023/+csgo_econ_action_preview%20S76561198185748194A28433491397D9821478250864647424', 'steam://rungame/730/76561202255233023/+csgo_econ_action_preview%20S76561198185748194A28315956209D14460612990892731053', 'steam://rungame/730/76561202255233023/+csgo_econ_action_preview%20S76561198185748194A28315874929D12397849034060993453', 'steam://rungame/730/76561202255233023/+csgo_econ_action_preview%20S76561198185748194A28229768155D11574396181669127541', 'steam://rungame/730/76561202255233023/+csgo_econ_action_preview%20S76561198185748194A27955299910D14756333338738051571']
+    const priceTypes = ['0', '0', '1', '0', '0', '2', '0', '0'];
+    const prices = ['2977130000000000', '8933480000000000', '27800000', '44661960000000000', '89323910000000000', '457000000', '1489077140000000000', '1489077140000000000'];
+    const names = ['R8 Revolver | Bone Mask (Field-Tested)', 'Negev | Bulkhead (Minimal Wear)', 'Negev | Army Sheen (Minimal Wear)', 'MAC-10 | Nuclear Garden (Field-Tested)', 'Galil AR | Rocket Pop (Factory New)', 'StatTrak™ AK-47 | Slate (Factory New)', '★ Driver Gloves | Rezan the Red (Minimal Wear)', '★ Driver Gloves | Rezan the Red (Minimal Wear)']
+    const weaponTypes = ['R8 Revolver', 'Negev', 'Negev', 'MAC-10', 'Galil AR', 'AK-47', 'Driver Gloves', 'Driver Gloves'];
+    const imgs = ['https://media.steampowered.com/apps/730/icons/econ/default_generated/weapon_revolver_sp_tape_light_large.c8f9124ff70ca2a6e8867920cd39e4fb7308ac87.png', 'https://media.steampowered.com/apps/730/icons/econ/default_generated/weapon_negev_hy_ducts_yellow_light_large.9d9335325a4a696ec6c2ef704ec1d4b3112c8c87.png', 'https://media.steampowered.com/apps/730/icons/econ/default_generated/weapon_negev_am_army_shine_light_large.884085f4a13b786f0ac7234d616ff01a848f28d5.png', 'https://media.steampowered.com/apps/730/icons/econ/default_generated/weapon_mac10_am_nuclear_skulls3_mac10_light_large.467b325065522e5248247cf125bec257cdb66902.png', 'https://media.steampowered.com/apps/730/icons/econ/default_generated/weapon_galilar_cu_galilar_particles_light_large.8732f64d53dbc9b0c732641655d4f99124d8cacc.png', 'https://media.steampowered.com/apps/730/icons/econ/default_generated/weapon_ak47_gs_ak47_professional_light_large.d09d623d0a725c63e8a3905f66bba41ba2ed59e8.png', 'https://media.steampowered.com/apps/730/icons/econ/default_generated/slick_gloves_slick_rezan_light_large.642934831085e8715a7e8072614f71f9fc0f205e.png', 'https://media.steampowered.com/apps/730/icons/econ/default_generated/slick_gloves_slick_rezan_light_large.642934831085e8715a7e8072614f71f9fc0f205e.png']
+    const inspctLink = ['steam://rungame/730/76561202255233023/+csgo_econ_action_preview%20S76561198185748194A28591758742D769815130885352460', 'steam://rungame/730/76561202255233023/+csgo_econ_action_preview%20S76561198185748194A28492787574D16143929557675144168', 'steam://rungame/730/76561202255233023/+csgo_econ_action_preview%20S76561198185748194A28433491397D9821478250864647424', 'steam://rungame/730/76561202255233023/+csgo_econ_action_preview%20S76561198185748194A28315956209D14460612990892731053', 'steam://rungame/730/76561202255233023/+csgo_econ_action_preview%20S76561198185748194A28315874929D12397849034060993453', 'steam://rungame/730/76561202255233023/+csgo_econ_action_preview%20S76561198185748194A28229768155D11574396181669127541', 'steam://rungame/730/76561202255233023/+csgo_econ_action_preview%20S76561198185748194A27955299910D14756333338738051571', 'steam://rungame/730/76561202255233023/+csgo_econ_action_preview%20S76561198185748194A27955299910D14756333338738051571']
     const skinInfo = [
         {
             floatValues: '[0.8, 0.06, 0.35223010182380676]',
@@ -80,8 +80,13 @@ describe("TradeFactory", function () {
             paintSeed: '556',
             paintIndex: '10069'
         },
+        {
+            floatValues: '[0.8, 0.06, 0.10778621584177017]',
+            paintSeed: '556',
+            paintIndex: '10069'
+        },
     ]
-    const assetIds = ['GG1', '28492787574', 'GG2', '28315956209', '28315874929', '28229768155', '27955299910'];
+    const assetIds = ['GG1', '28492787574', 'GG2', '28315956209', '28315874929', '28229768155', '27955299910', '3213213'];
     
     beforeEach(async function () {
         [deployer, council, keeperNodeAddress, user1, user2] = await ethers.getSigners();
