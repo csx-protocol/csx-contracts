95465f4f02f432cf6878b861bf431e5f31e64ad7
charliedevcode <charliedevcode@gmail.com>
2023-11-30 16:26:16 +0100

    [I-01] Some Contracts Are Missing License (2)

--- contracts/TradeFactory/CSXTradeFactory.sol
+++ contracts/TradeFactory/CSXTradeFactory.sol
@@ -1,4 +1,4 @@
-// //SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: MIT
 pragma solidity 0.8.18;
 
 import {CSXTrade, PriceType, TradeUrl, SkinInfo, Sticker} from "../Trade/CSXTrade.sol";
--- contracts/TradeFactory/TradeFactoryBase.sol
+++ contracts/TradeFactory/TradeFactoryBase.sol
@@ -1,4 +1,4 @@
-// //SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: MIT
 pragma solidity 0.8.18;
 
 import {ReentrancyGuard} from "@openzeppelin/contracts/security/ReentrancyGuard.sol";
